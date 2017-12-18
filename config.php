43a2e43173810b5020f88746b9d71d29c1596e30
Chairuddinsyah <chairuddinsyah@10.0.4.13>
2017-12-18 14:24:14 +0700

    Update Commit

--- config.php
+++ config.php
@@ -4,4 +4,5 @@ include("/var/www/mysql-config2.php");
 
 $mydatabase = $IS_DEVELOPMENT ? "advframeworkdev" : "advframework";
 
+$url_static_time = "http://alegrium5.alegrium.com/advframework/cloudsave/";
 
